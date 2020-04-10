cd12f7f21728c311eeb796e3e56ba0ebf8cf48bc
Dominik <domix852@gmail.com>
2020-04-10 09:08:32 +0200

    Simple shopping simulator

---
+++ DemoShop/DemoShop.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace DemoShop
+{
+    partial class DemoShop
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.itemsListBoxLabel = new System.Windows.Forms.Label();
+            this.itemsListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addToCart = new System.Windows.Forms.Button();
+            this.shoppingCartlistBox = new System.Windows.Forms.ListBox();
+            this.shoppingCartListBoxLabel = new System.Windows.Forms.Label();
+            this.makePurchase = new System.Windows.Forms.Button();
+            this.vendorsListBox = new System.Windows.Forms.ListBox();
+            this.vendorList = new System.Windows.Forms.Label();
+            this.storePayoutLabel = new System.Windows.Forms.Label();
+            this.storePayoutValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // itemsListBoxLabel
+            // 
+            this.itemsListBoxLabel.AutoSize = true;
+            this.itemsListBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.itemsListBoxLabel.Location = new System.Drawing.Point(97, 110);
+            this.itemsListBoxLabel.Name = "itemsListBoxLabel";
+            this.itemsListBoxLabel.Size = new System.Drawing.Size(146, 29);
+            this.itemsListBoxLabel.TabIndex = 0;
+            this.itemsListBoxLabel.Text = "Store Items";
+            // 
+            // itemsListBox
+            // 
+            this.itemsListBox.FormattingEnabled = true;
+            this.itemsListBox.ItemHeight = 29;
+            this.itemsListBox.Location = new System.Drawing.Point(105, 142);
+            this.itemsListBox.Name = "itemsListBox";
+            this.itemsListBox.Size = new System.Drawing.Size(231, 178);
+            this.itemsListBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(97, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SHOP";
+            // 
+            // addToCart
+            // 
+            this.addToCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.addToCart.Location = new System.Drawing.Point(413, 209);
+            this.addToCart.Name = "addToCart";
+            this.addToCart.Size = new System.Drawing.Size(160, 41);
+            this.addToCart.TabIndex = 3;
+            this.addToCart.Text = "Add To Cart ->";
+            this.addToCart.UseVisualStyleBackColor = true;
+            this.addToCart.Click += new System.EventHandler(this.addToCart_Click);
+            // 
+            // shoppingCartlistBox
+            // 
+            this.shoppingCartlistBox.FormattingEnabled = true;
+            this.shoppingCartlistBox.ItemHeight = 29;
+            this.shoppingCartlistBox.Location = new System.Drawing.Point(638, 142);
+            this.shoppingCartlistBox.Name = "shoppingCartlistBox";
+            this.shoppingCartlistBox.Size = new System.Drawing.Size(231, 178);
+            this.shoppingCartlistBox.TabIndex = 5;
+            // 
+            // shoppingCartListBoxLabel
+            // 
+            this.shoppingCartListBoxLabel.AutoSize = true;
+            this.shoppingCartListBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.shoppingCartListBoxLabel.Location = new System.Drawing.Point(630, 110);
+            this.shoppingCartListBoxLabel.Name = "shoppingCartListBoxLabel";
+            this.shoppingCartListBoxLabel.Size = new System.Drawing.Size(180, 29);
+            this.shoppingCartListBoxLabel.TabIndex = 4;
+            this.shoppingCartListBoxLabel.Text = "Shopping Cart";
+            // 
+            // makePurchase
+            // 
+            this.makePurchase.Location = new System.Drawing.Point(743, 327);
+            this.makePurchase.Name = "makePurchase";
+            this.makePurchase.Size = new System.Drawing.Size(125, 47);
+            this.makePurchase.TabIndex = 6;
+            this.makePurchase.Text = "Purchase";
+            this.makePurchase.UseVisualStyleBackColor = true;
+            this.makePurchase.Click += new System.EventHandler(this.makePurchase_Click);
+            // 
+            // vendorsListBox
+            // 
+            this.vendorsListBox.FormattingEnabled = true;
+            this.vendorsListBox.ItemHeight = 29;
+            this.vendorsListBox.Location = new System.Drawing.Point(105, 421);
+            this.vendorsListBox.Name = "vendorsListBox";
+            this.vendorsListBox.Size = new System.Drawing.Size(231, 178);
+            this.vendorsListBox.TabIndex = 8;
+            this.vendorsListBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // vendorList
+            // 
+            this.vendorList.AutoSize = true;
+            this.vendorList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.vendorList.Location = new System.Drawing.Point(97, 389);
+            this.vendorList.Name = "vendorList";
+            this.vendorList.Size = new System.Drawing.Size(145, 29);
+            this.vendorList.TabIndex = 7;
+            this.vendorList.Text = "Vendor List";
+            this.vendorList.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // storePayoutLabel
+            // 
+            this.storePayoutLabel.AutoSize = true;
+            this.storePayoutLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.storePayoutLabel.Location = new System.Drawing.Point(638, 389);
+            this.storePayoutLabel.Name = "storePayoutLabel";
+            this.storePayoutLabel.Size = new System.Drawing.Size(125, 25);
+            this.storePayoutLabel.TabIndex = 9;
+            this.storePayoutLabel.Text = "Store Payout";
+            // 
+            // storePayoutValue
+            // 
+            this.storePayoutValue.AutoSize = true;
+            this.storePayoutValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.storePayoutValue.Location = new System.Drawing.Point(786, 389);
+            this.storePayoutValue.Name = "storePayoutValue";
+            this.storePayoutValue.Size = new System.Drawing.Size(66, 25);
+            this.storePayoutValue.TabIndex = 10;
+            this.storePayoutValue.Text = "$0,00 ";
+            // 
+            // DemoShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1244, 653);
+            this.Controls.Add(this.storePayoutValue);
+            this.Controls.Add(this.storePayoutLabel);
+            this.Controls.Add(this.vendorsListBox);
+            this.Controls.Add(this.vendorList);
+            this.Controls.Add(this.makePurchase);
+            this.Controls.Add(this.shoppingCartlistBox);
+            this.Controls.Add(this.shoppingCartListBoxLabel);
+            this.Controls.Add(this.addToCart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.itemsListBox);
+            this.Controls.Add(this.itemsListBoxLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "DemoShop";
+            this.Text = "Demo Shop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label itemsListBoxLabel;
+        private System.Windows.Forms.ListBox itemsListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addToCart;
+        private System.Windows.Forms.ListBox shoppingCartlistBox;
+        private System.Windows.Forms.Label shoppingCartListBoxLabel;
+        private System.Windows.Forms.Button makePurchase;
+        private System.Windows.Forms.ListBox vendorsListBox;
+        private System.Windows.Forms.Label vendorList;
+        private System.Windows.Forms.Label storePayoutLabel;
+        private System.Windows.Forms.Label storePayoutValue;
+    }
+}
+
---
+++ DemoShop/DemoShop.cs
@@ -0,0 +1,123 @@
+﻿using ShopLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoShop
+{
+    public partial class DemoShop : Form
+    {
+        private Store store = new Store();
+        private List<Item> shoppingCartData = new List<Item>();
+        BindingSource itemsBinding = new BindingSource();
+        BindingSource cartBinding = new BindingSource();
+        BindingSource vendorsBinding = new BindingSource();
+        private decimal storeProfit = 0;
+        public DemoShop()
+        {
+            InitializeComponent();
+            SetupData();
+            itemsBinding.DataSource = store.Items.Where(x => x.Sold==false).ToList();
+            itemsListBox.DataSource = itemsBinding;
+
+            itemsListBox.DisplayMember = "Show";
+            itemsListBox.ValueMember = "Show";
+
+            cartBinding.DataSource = shoppingCartData;
+            shoppingCartlistBox.DataSource = cartBinding;
+
+            shoppingCartlistBox.DisplayMember = "Show";
+            shoppingCartlistBox.ValueMember = "Show";
+
+            vendorsBinding.DataSource = store.Vendors;
+            vendorsListBox.DataSource = vendorsBinding;
+
+            vendorsListBox.DisplayMember = "Show";
+            vendorsListBox.ValueMember = "Show";
+
+        }
+
+        private void SetupData()
+        {
+            store.Vendors = new List<Vendor>();
+            store.Vendors.Add(new Vendor { FirstName = "Axel", LastName = "Ryan"});
+            store.Vendors.Add(new Vendor { FirstName = "Michael", LastName = "Stanley"});
+
+            store.Items.Add(new Item
+            {
+                Title = "Moby Dick",
+                Description = "A book about a whale",
+                Price = 4.50M,
+                Owner = store.Vendors[0]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "A tale of two cities",
+                Description = "A book about a revolution",
+                Price = 3.80M,
+                Owner = store.Vendors[1]
+            });
+            store.Items.Add(new Item
+            {
+                Title = "The Witcher",
+                Description = "A book about the butcher of Blaviken",
+                Price = 12.20M,
+                Owner = store.Vendors[1]
+            });
+            store.Items.Add(new Item
+            {
+                Title ="The lord of the rings",
+                Description = "A book about the good and the evil",
+                Price = 7.40M,
+                Owner = store.Vendors[0]
+            });
+
+            store.Name = "";
+
+
+        }
+
+        private void addToCart_Click(object sender, EventArgs e)
+        {
+            Item selectedItem = (Item)itemsListBox.SelectedItem;
+            shoppingCartData.Add(selectedItem);
+            cartBinding.ResetBindings(false);
+
+        }
+
+        private void makePurchase_Click(object sender, EventArgs e)
+        {
+            foreach  (Item item in shoppingCartData)
+            {
+                item.Sold = true;
+                item.Owner.PaymentDue += (decimal)item.Owner.Commission * item.Price;
+                storeProfit += (1 - (decimal)item.Owner.Commission) * item.Price;
+            }
+            shoppingCartData.Clear(); 
+            itemsBinding.DataSource = store.Items.Where(x => x.Sold == false).ToList();
+
+            storePayoutValue.Text = string.Format("${0}", storeProfit);
+
+            cartBinding.ResetBindings(false);
+            itemsBinding.ResetBindings(false);
+            vendorsBinding.ResetBindings(false);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
